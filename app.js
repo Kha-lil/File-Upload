186e9ed0443fe2311033ff061d542f9532ec61dd
Ikumapayi Khalilullah Olaniyi <ikumapayiolaniyi007@gmail.com>
2022-10-10 17:24:38 +0100

    chore: project finish up

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express');
+const path = require("path");
+
+const express = require("express");
 const app = express();
 
+const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 // database
-const connectDB = require('./db/connect');
+const connectDB = require("./db/connect");
+
+//product router
+const productRouter = require("./routes/productRoutes");
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.get('/', (req, res) => {
-  res.send('<h1>File Upload Starter</h1>');
+app.use(express.json());
+app.use(fileUpload({ useTempFiles: true }));
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+app.use("/api/v1/products", productRouter);
+app.get("/", (req, res) => {
+  res.send("<h1>File Upload Starter</h1>");
 });
 
 // middleware
